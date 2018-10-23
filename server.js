a56aefbd1e4f7fef2b91437d0510265013ba9643
sunyu786507754 <sunyu786507754@163.com>
2018-10-23 19:18:29 +0800

    20181023提交

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ server.listen(3000,function(){
     console.log('Server listening on port 3000');
 });
 
-var chatServer=require('./lib/chat_server.js');
-chatServer(server);
+var chatServer=require('./lib/chat_server');
+chatServer.listen(server);
